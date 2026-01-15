6aaa8df77485a4f6fea7c855d7206f8272fa06d2
Daisy Kim <doyeonyyy@gmail.com>
2026-01-16 05:33:32 +0900

    Add full project

---
+++ TrainingMindMuscleTests/TrainingMindMuscleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingMindMuscleTests.swift
+//  TrainingMindMuscleTests
+//
+//  Created by Doyeon Kim on 4/16/25.
+//
+
+import Testing
+@testable import TrainingMindMuscle
+
+struct TrainingMindMuscleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
